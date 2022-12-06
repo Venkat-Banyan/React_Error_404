15f1d4610a50fd0dc56fb6a9d19c345d1e449cf5
Venkat G <96160078+Venkat-Banyan@users.noreply.github.com>
2022-12-06 11:44:57 +0530

    Add files via upload

---
+++ WhatsApp_Web_React/build/precache-manifest.2e69f1005e3b7a5535f3974782b8979f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "599d6b6dc50198a61010",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/css/main.ad6b88d4.chunk.css"
+  },
+  {
+    "revision": "599d6b6dc50198a61010",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/js/main.599d6b6d.chunk.js"
+  },
+  {
+    "revision": "6ac9694872e871f8aec1",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/js/1.6ac96948.chunk.js"
+  },
+  {
+    "revision": "4783162d81f6d568572e",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/js/runtime~main.4783162d.js"
+  },
+  {
+    "revision": "d08b1ebfeeb897bfc905e5dae2009129",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/media/WhatsApp_Logo.d08b1ebf.svg"
+  },
+  {
+    "revision": "0c6ec69b054fdeb31cf3e5e10290fd8e",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/static/media/whatsapp_download_bg.0c6ec69b.png"
+  },
+  {
+    "revision": "fbb83139726cc0ef9910c2f6796bb9aa",
+    "url": "/venkat/React_404/Whatsapp/Whatsapp_Web/index.html"
+  }
+];
